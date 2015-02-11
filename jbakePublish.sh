6fa6fb2dc98795625d6c8a5b02518a5b78b5bc41
Lukasz Siwinski <lsiwinski@gmail.com>
2015-02-11 23:06:15 +0100

    configuration: publishing script updated

--- jbakePublish.sh
+++ jbakePublish.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+shopt -s extglob
+cd ../output/. && rm -rvf !(\.git) && cd ../content
 jbake . ../output/. && cd ../output && git add . -A && git commit -m "jbake: site update from master branch" && git push origin gh-pages && cd ../content
